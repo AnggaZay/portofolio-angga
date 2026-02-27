18b77ef48f4810dd6a806b96aeb111f4da6e420d
unknown <anggazaidan4@gmail.com>
2026-02-27 07:28:58 +0700

    feat: add tabakery erp showcase module

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  // Tambahin ini buat "ngebuka pintu" buat iframe Figma
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-src 'self' https://www.figma.com https://embed.figma.com;",
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
